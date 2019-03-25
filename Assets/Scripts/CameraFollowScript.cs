3c8bca5223e0afe81dfae6f3e2d6c814f4deb699
Kazkel <matthewhansson@yahoo.com>
2019-03-25 23:53:41 +0000

    changed camera to smoothly follow
    added follow Lag parameter to camera, to smooth out and reposition it

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraFollowScript : MonoBehaviour {
 
     public GameObject target; //Creates a variable called target
+    public float followLag = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,9 +13,9 @@ public class CameraFollowScript : MonoBehaviour {
 	}
 	
 	// Updates the camera position to follow the player
-	void Update () {
+	void LateUpdate () {
+        Vector3 targetPos = new Vector3(target.transform.position.x + 6.6f, target.transform.position.y + 2f, transform.position.z);
 
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); 
-		
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * followLag);
 	}
 }
