978c627f1e842c4a7a7cc02fc7f3a2cfdb3eb7dd
Kazkel <matthewhansson@yahoo.com>
2019-03-26 00:00:10 +0000

    Revert "changed camera to smoothly follow"
    This reverts commit 3c8bca5223e0afe81dfae6f3e2d6c814f4deb699.

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraFollowScript : MonoBehaviour {
 
     public GameObject target; //Creates a variable called target
-    public float followLag = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,9 +12,9 @@ public class CameraFollowScript : MonoBehaviour {
 	}
 	
 	// Updates the camera position to follow the player
-	void LateUpdate () {
-        Vector3 targetPos = new Vector3(target.transform.position.x + 6.6f, target.transform.position.y + 2f, transform.position.z);
+	void Update () {
 
-        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * followLag);
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z); 
+		
 	}
 }
