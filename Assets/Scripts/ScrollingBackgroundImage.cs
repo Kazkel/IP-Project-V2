568f9e9a9ee98444a2cfcad049b85244228c81fe
Kazkel <matthewhansson@yahoo.com>
2019-03-26 00:53:50 +0000

    added scrolling background to camera prefab
    added scrolling background plane to project, changed the B1 image sprite wrap mode from clamp to wrap

---
+++ Assets/Scripts/ScrollingBackgroundImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackgroundImage : MonoBehaviour
+{
+    public float scrollX = 0.01f;
+    public float scrollY = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offsetX = transform.position.x * scrollX;
+        float offsetY = transform.position.y * scrollY;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
