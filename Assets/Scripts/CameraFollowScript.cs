3277ed6fcbdc9a5ec2b9d84cb7fcae91c15c598b
Kazkel <matthewhansson@yahoo.com>
2019-04-03 11:26:05 +0100

    fixed camera jitter
    enabled interpolate on player rigidbody, changed to camera  smoothDamp

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CameraFollowScript : MonoBehaviour {
 
     public GameObject target; //Creates a variable called target
-    public float followLag = 5f;
+    public float followLag = 0.3f;
     public float xOffset = 4.05f;
     public float yOffset = -3.0f;
     public float cameraSize = 5.95f;
     public float shakeTimer = 0f;
     public float shakeWeight = 0.1f;
+
+    private Vector3 currVelocity = Vector3.zero;
 
     // Use this for initialization
     void Start () {
@@ -20,7 +22,8 @@ public class CameraFollowScript : MonoBehaviour {
 	// Updates the camera position to follow the player
 	void LateUpdate () {        
         Vector3 targetPosition = new Vector3(target.transform.position.x + xOffset, target.transform.position.y + yOffset, transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * followLag);
+        //transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * followLag);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currVelocity, followLag);
         
 
         if(shakeTimer > 0) {
