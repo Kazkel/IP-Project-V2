e07e8e2895368e11ba9c9b403f86ae1744bfbfb9
Kazkel <matthewhansson@yahoo.com>
2019-04-09 14:03:15 +0100

    camera backgroundscroll improvements, moved all of prototype level 2 up by 57 units

--- Assets/Scripts/ScrollingBackgroundImage.cs
+++ Assets/Scripts/ScrollingBackgroundImage.cs
@@ -6,6 +6,8 @@ public class ScrollingBackgroundImage : MonoBehaviour
 {
     public float scrollX = 0.01f;
     public float scrollY = 0.01f;
+    public float startingOffsetX = 0f;
+    public float startingOffsetY = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,8 @@ public class ScrollingBackgroundImage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float offsetX = transform.position.x * scrollX;
-        float offsetY = transform.position.y * scrollY;
+        float offsetX = (transform.position.x + startingOffsetX ) * scrollX;
+        float offsetY = (transform.position.y + startingOffsetY ) * scrollY;
         GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
     }
 }
